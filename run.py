ecf0cb41ba5e33ec3c1e72b659aefa339c290287
lastchancebar <darinaduffykelly@gmail.com>
2022-05-02 11:21:55 +0000

    function to generate easy medium and difficult sudoku puzzles

--- run.py
+++ run.py
@@ -3,7 +3,6 @@ import random
 
 #Prints sudoku array in familiar format
 
-
 def Show_Board(board):
     for i in range(9): #i represents row
         for j in range(9): #j represents column
@@ -32,3 +31,59 @@ def find_next_empty(board):
                 return result
                 result = np.array(-1,-1,0])
                 return result
+
+# this function checks the validity og a number at a given position
+#according to sudoku rules.
+
+def check_input_validity(board,row,col,num):
+    row_start=(row // 3) * 3
+    col_start=(col // 3) * 3
+    if num in board[:, col] or num in board[row,:]:
+        return False
+    if num in board[row_start:row_start + 3, col_start: + 3]:
+        return False
+    return True    
+
+#generates an unsolved sudoku board based on required difficulty
+
+
+def unsolved_puzzle(board, difficulty):
+    count, done = 0, False
+    if difficulty == "Easy":
+        print("Generating Easy Puzzle...\n\n")
+        upper_limit=35
+    elif difficulty == "Medium":
+        print("Generating puzzle of medium difficulty...\n\n")
+        upper_limit=41 
+    else: 
+        print("Generating difficult puzzle...\n\n")
+        upper_limit=47
+    while True:
+        i = random.randint(0,8)
+        j = random.randint(0,8)  
+        if count <=upper_limit:
+            if board[i,j] != 0:
+                not_check= board[i,j]
+                board[i,j] = 0
+                board_copy=board
+                if solve_sudoku(board_copy, not_check):
+                    board[i,j]= not_check
+                    continue 
+                row_start=(i // 3) * 3
+                col_start=(j // 3) * 3
+                if difficulty == "Easy":
+                    if np.count_nonzero(board[row_start:row_start+3, col_start: col_start+3]) < 5:
+                        board[i,j]=not_check
+                        continue
+                elif difficulty == "Medium":
+                    if np.count_nonzero(board[row_start:row_start+3, col_start: col_start+3]) < 4:
+                        board[i,j]=not_check
+                        continue
+                else:
+                    if np.count_nonzero(board[row_start:row_start+3, col_start: col_start+3]) < 3:
+                        board[i,j]=not_check
+                count+=1
+        else:
+            done=True
+            break        
+
